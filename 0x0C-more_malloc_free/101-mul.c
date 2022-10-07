23baf808494f634e5eb3ced45b043ce86ed74e29
Ndubuisi O <foundictsolutions@gmail.com>
2022-10-07 13:37:46 +0100

    corrected 0x0c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,69 +3,17 @@
 #include <stdio.h>
 #include <ctype.h>
 
-/**
- * _is_zero - determines if any number is zero
- * @argv: argument vector.
- *
- * Return: no return.
- */
-void _is_zero(char *argv[])
-{
-	int i, isn1 = 1, isn2 = 1;
-
-	for (i = 0; argv[1][i]; i++)
-		if (argv[1][i] != '0')
-		{
-			isn1 = 0;
-			break;
-		}
-
-	for (i = 0; argv[2][i]; i++)
-		if (argv[2][i] != '0')
-		{
-			isn2 = 0;
-			break;
-		}
-
-	if (isn1 == 1 || isn2 == 1)
-	{
-		printf("0\n");
-		exit(0);
-	}
-}
 
 /**
- * _initialize_array - set memery to zero in a new array
- * @num1: first number.
- * @num2: second number.
+ * mul - Return a multiplation of two integer numbers
+ * @numone: first number.
+ * @numtwo: length of the char array.
  *
- * Return: the result of the multiplication.
+ * Return: int.
  */
-unsigned long mul(int num1, int num2)
+unsigned long mul(int numone, int numtwo)
 {
-	return (num1*num2);
-}
-
-/**
- * _checknum - determines length of the number
- * and checks if number is in base 10.
- * @argv: arguments vector.
- * @n: row of the array.
- *
- * Return: length of the number.
- */
-int _checknum(char *argv[], int n)
-{
-	int ln;
-
-	for (ln = 0; argv[n][ln]; ln++)
-		if (!isdigit(argv[n][ln]))
-		{
-			printf("Error\n");
-			exit(98);
-		}
-
-	return (ln);
+	return (numone*numtwo);
 }
 
 /**
@@ -74,20 +22,28 @@ int _checknum(char *argv[], int n)
  * @argc: number of arguments.
  * @argv: arguments vector.
  *
- * Return: 0 - success.
+ * Return: int
  */
 int main(int argc, char *argv[])
 {
-	int ln1, ln2, numTwo, numOne, *result;
-
+	int x, y;
+unsigned long result;
 
 	if (argc != 3)
+	{
 		printf("Error\n"), exit(98);
-	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
-	_is_zero(argv), numTwo = ln1 + ln2, numOne = malloc(numTwo + 1);
-	if (numOne == NULL)
-		printf("Error\n"), exit(98);
-	result= mul(numOne, numTwo);
+	}
+	for(x =1;x< argc;x++){
+		for(y=0;argv[x][y] != '\0';y++){
+			if(argv[x][y] > 57 || argv[x][y]<48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+	
+	result= mul(argv[1], argv[2]);
 	
 	printf("%d\n", result);
 	return (0);
